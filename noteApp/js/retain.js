e254fa960a4cad3b15144ef1f7352a9cbf46b63f
LevKazar <lev.kazar@gmail.com>
2017-10-07 19:11:35 +0200

    removed docstrings

--- noteApp/js/retain.js
+++ noteApp/js/retain.js
@@ -1,60 +1,26 @@
 $(function() {
-    /**
-     * 
-     * 
-     * @class Model
-     */
+    
     class Model {
-        /**
-         * 
-         * 
-         * @static
-         * @memberof Model
-         */
+
         static init() {
             if (!localStorage.notes) {
                 localStorage.notes = JSON.stringify([]);
             }
         }
 
-        /**
-         * 
-         * 
-         * @static
-         * @param {any} obj 
-         * @memberof Model
-         */
         static add(obj) {
             let data = JSON.parse(localStorage.notes);
             data.push(obj);
             localStorage.notes = JSON.stringify(data);
         }
 
-        /**
-         * 
-         * 
-         * @static
-         * @return {string}
-         * @memberof Model
-         */
         static getAllNotes() {
             return JSON.parse(localStorage.notes);
         }
     };
 
-    /**
-     * 
-     * 
-     * @class Octopus
-     */
     class Octopus {
-        /**
-         * 
-         * 
-         * @static
-         * @param {any} noteStr 
-         * @memberof Octopus
-         */
+
         static addNewNote(noteStr) {
             Model.add({
                 content: noteStr,
@@ -63,42 +29,18 @@ $(function() {
             View.render();
         }
 
-        /**
-         * 
-         * 
-         * @static
-         * @return {array} 
-         * @memberof Octopus
-         */
         static getNotes() {
             return Model.getAllNotes();
         }
 
-        /**
-         * 
-         * 
-         * @static
-         * @memberof Octopus
-         */
         static init() {
             Model.init();
             View.init();
         }
     };
 
-
-    /**
-     * 
-     * 
-     * @class View
-     */
     class View {
-        /**
-         * 
-         * 
-         * @static
-         * @memberof View
-         */
+
         static init() {
             this.noteList = $('#notes');
             let newNoteForm = $('#new-note-form');
@@ -111,12 +53,6 @@ $(function() {
             View.render();
         }
 
-        /**
-         * 
-         * 
-         * @static
-         * @memberof View
-         */
         static render() {
             let htmlStr = '';
             Octopus.getNotes().forEach(function(note) {
